29adb5e3efca505aeefb8ef1f85739eff6bb36ac
Victor Lamoine <victor.lamoine@gmail.com>
2016-04-18 16:52:17 +0200

    Hide PPA and apt-update output

--- .travis_pcl.sh
+++ .travis_pcl.sh
@@ -3,8 +3,8 @@
 download_server="http://graissmoto.legtux.org/travis_ci"
 
 # Dependencies
-sudo add-apt-repository -y ppa:v-launchpad-jochen-sprickerhof-de/pcl
-sudo apt-get update
+sudo add-apt-repository -y ppa:v-launchpad-jochen-sprickerhof-de/pcl > /dev/null
+sudo apt-get update > /dev/null
 sudo apt-get -qq install -y libeigen3-dev libflann-dev libboost-all-dev libopenni-dev libopenni2-dev
 
 # Download and extract pcl 1.8.0
